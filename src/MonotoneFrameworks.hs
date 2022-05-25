5a9a69aed982bef7a9be5de6fec0e2a94e9074f0
rednaZ <p.rednaz@googlemail.com>
2022-05-25 17:19:57 +0200

    MonotoneFrameworks. fix work list bug

--- src/MonotoneFrameworks.hs
+++ src/MonotoneFrameworks.hs
@@ -28,64 +28,6 @@ data MonotoneFramework propertySpace =
       transferFunctions :: Map Label (propertySpace -> propertySpace)
     }
 
--- | intraprocedural MFP solution
--- | warning. partial. 'transferFunctions' must cover all labels.
---
--- warning. termination is only guaranteed if 'propertySpace' satisfies the
--- /Ascending Chain Condition/.
-mfpSolution ::
-  forall propertySpace.
-  (BoundedSemiLattice propertySpace) =>
-  MonotoneFramework propertySpace ->
-  -- | \(\mathrm{Analysis}_\circ\) and \(\mathrm{Analysis}_\bullet\)
-  (Map Label propertySpace, Map Label propertySpace)
-mfpSolution
-  (MonotoneFramework flow extremalLabels extremalValue transferFunctions)
-  = (analysisEntry, M.mapWithKey transferFunction analysisEntry)
-  where
-    (_, analysisEntry) =
-      iterateFinite step (initialAnalysis, initialWorkList)
-    initialAnalysis =
-      M.fromSet (const extremalValue) extremalLabels
-      <> -- left biased union
-      M.fromSet (const bottom) (M.keysSet flow <> fold flow)
-    initialWorkList =
-      foldMap (\(l, ls) -> (l,) <$> toList ls) (M.toList flow)
-    step ::
-      -- | pair of current analysis and worklist
-      (Map Label propertySpace, [(Label, Label)]) ->
-      -- | either result in case of termination or new state
-      Either
-        (Map Label propertySpace) -- terminate
-        (Map Label propertySpace, [(Label, Label)])
-    step (analysisOld, (l, l') : workListRest)
-      |
-        transferFunction l (analysisLookup l analysisOld)
-        `lessOrEquals`
-        analysisLookup l' analysisOld
-        = Right (analysisOld, workListRest)
-      | otherwise =
-        Right $
-        (
-          M.adjust
-            (<> transferFunction l (analysisLookup l analysisOld))
-            l'
-            analysisOld
-          ,
-          fmap (l',) $ toList $ fromMaybe S.empty $ M.lookup l' flow
-        )
-    step (analysis, []) = Left analysis -- terminate
-    -- | warning. partial
-    transferFunction :: Label -> propertySpace -> propertySpace
-    transferFunction l =
-      fromMaybe (error $ "transfer function missing for " <> show l) $
-      M.lookup l transferFunctions
-    -- | warning. partial
-    analysisLookup :: Label -> Map Label propertySpace -> propertySpace
-    analysisLookup =
-      fromMaybe (error "impossible. initialAnalysis covers all labels") .:
-      M.lookup
-
 -- invariant. call label is functionally dependent on the return label
 newtype InterproceduralFragment propertySpace =
   InterproceduralFragment
@@ -146,9 +88,9 @@ mfpSolution'
             (case lookupCall l' interproceduralFragment of
               Nothing -> []
               Just (returnLabel, _) -> outgoingFlow returnLabel
-            )
-            <>
-            outgoingFlow l'
+            ) <>
+            outgoingFlow l' <>
+            workListRest
           )
         )
       where
